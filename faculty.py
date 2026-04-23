dda47c05a83c7209504c62fbea423a878d85c3e5
marianaportosaraiva <up202505155@up.pt>
2026-04-23 12:12:08 +0100

    Add files via upload

---
+++ faculty.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 21:20:46 2026
+
+@author: mariana
+"""
+
+from classes.gclass import Gclass
+
+class Faculty(Gclass):
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    att = ['_id', '_info', '_university_id']
+    header = 'Faculties'
+    des = ['Faculty ID', 'Faculty Info', 'University ID']
+
+    def __init__(self, id, info, university_id):
+        super().__init__()
+        id = Faculty.get_id(id)
+        self._id = id
+        self._info = info
+        self._university_id = university_id
+        
+        Faculty.obj[id] = self
+        Faculty.lst.append(id)
+
+    @property
+    def id(self): return self._id
+    @id.setter
+    def id(self, id): self._id = id
+
+    @property
+    def info(self): return self._info
+    @info.setter
+    def info(self, info): self._info = info
+
+    @property
+    def university_id(self): return self._university_id
+    @university_id.setter
+    def university_id(self, university_id): self._university_id = university_id
